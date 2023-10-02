23f2719c5316acbfd13b528348cebfb38d79c8be
dudleylim <dudleylim@gmail.com>
2023-10-02 08:12:32 +0800

    route params

--- frontend/src/app/comm-class/pages/view-specific/view-specific.component.ts
+++ frontend/src/app/comm-class/pages/view-specific/view-specific.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-view-specific',
   templateUrl: './view-specific.component.html',
   styleUrls: ['./view-specific.component.css']
 })
-export class ViewSpecificComponent {
+export class ViewSpecificComponent implements OnInit {
+  id: Number = 0;
 
+  constructor(private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.id = +Number(params.get('id'));
+    });
+    // this.id = Number(this.route.snapshot.paramMap.get('id'));
+  }
 }
